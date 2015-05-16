7293a50a5fc98fdba8c6d64a25799c1879c45d6c
Roberto J. Alcalá Sánchez <roberto.alcala@gmail.com>
2015-05-16 04:17:16 +0200

    Added Roxygen documentation and comments.

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,15 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+#' Create a matrix cache object.
+#'
+#' The cache matrix object contains the matrix passed by parameter but it also can cache its inverse.
+#' A list of methods for interfacing is returned and used by the \code{\link{cacheSolve}} matrix.
+#'
+#' @param  x A numeric matrix.
+#' @return A list of methods for interfacing with the cache matrix object.
+#'
+#' @examples
+#' mat <- makeCacheMatrix(matrix(rnorm(1000000), 1000, 1000))
+#'
+#' @seealso \code{\link{cacheSolve}}
 makeCacheMatrix <- function(x = matrix())
 {
     inv <- NULL
@@ -26,13 +33,26 @@ makeCacheMatrix <- function(x = matrix())
     )
 }
 
-
-## Write a short comment describing this function
-
+#' Invert a matrix stored in a cache matrix object.
+#'
+#' The inverse matrix stored inside the cache matrix object (build with
+#' \code{\link{makeCacheMatrix}}) is extracted. If it's null
+#' (not cached) it's calculated and stored for later quick access.
+#'
+#' @param  x   A cache matrix object.
+#' @param  ... An optional list of parameters (currently not used).
+#' @return The inverse of the matrix.
+#'
+#' @examples
+#' mat <- makeCacheMatrix(matrix(rnorm(1000000), 1000, 1000))
+#'
+#' @seealso \code{\link{makeCacheMatrix}}
 cacheSolve <- function(x, ...)
 {
     inv <- x$getInverse()
 
+    # If the inverse matrix is not available calculate it
+    # and store in the cache matrix object.
     if (is.null(inv))
     {
         inv <- solve(x$get())
